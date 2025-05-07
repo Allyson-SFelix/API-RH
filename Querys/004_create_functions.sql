3d19b64a42d10fb590537459439e1f2bc33fbbec
Allyson-SFelix <allysonstudy2021@gmail.com>
2025-05-06 21:12:02 -0300

    feat: adicionado querys de trigger e function sobre possivel alteracao do setor_id antigo comparado com o novo e alteracao no qtd_funcionarios

--- Querys/004_create_functions.sql
+++ Querys/004_create_functions.sql
@@ -19,4 +19,23 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
+
+CREATE FUNCTION mudar_qtd_funcionarios()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF OLD.setor_id IS DISTINCT FROM  NEW.setor_id THEN
+        
+        UPDATE setores 
+        SET qtd_funcionarios = GREATEST(0, qtd_funcionarios - 1)  PERMITE QUE CASO A SUBTRAÇÃO RESULTE EM NEGATIVO FIQUE 0
+        WHERE id = NEW.setor_id;
+
+        UPDATE setores
+        SET qtd_funcionarios=qtd_funcionarios + 1
+        WHERE id=OLD.setor_id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 */
--- Querys/005_create_Triggers.sql
+++ Querys/005_create_Triggers.sql
@@ -11,4 +11,10 @@ AFTER DELETE ON funcionarios
 FOR EACH ROW
 EXECUTE FUNCTION decrementar_qtd_funcionarios();
 
+
+CREATE TRIGGER trigger_muda_qtd_funcionarios
+AFTER UPDATE ON funcionarios
+FOR EACH ROW
+WHEN (OLD.setor_id IS DISTINCT FROM NEW.setor_id)  apenas quando setor_id antigo for diferente do novo quando houver um UPDATE
+EXECUTE FUNCTION mudar_qtd_funcionarios();
 */
